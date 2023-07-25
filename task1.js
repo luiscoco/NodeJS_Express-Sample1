a51070409d3b63bbba816a4620d21c1bf5daa357
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-25 15:33:11 +0200

    first commit

---
+++ task1.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+app.get("/notes", function(req,res) {
+   let notes = [
+       {text: "First note"},
+       {text: "Second note"},
+       {text: "Third note"}
+   ]
+   res.send(notes);
+});
+
+app.listen(3000);
